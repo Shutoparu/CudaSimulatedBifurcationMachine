7559deb2d894c59aec63c310cab5cc2321123cf1
Thomas Peng <thomaspeng0516@gmail.com>
2022-07-14 07:26:40 +0000

    implemented cpu version of sbm

--- main.py
+++ main.py
@@ -3,35 +3,52 @@ import numpy.ctypeslib as ctplib
 from ctypes import c_float, c_int, cdll, POINTER
 import time
 
-np.random.seed(1)
 
-dim = 15
-trotterNum = 4
-sweeps = 20
 
-qubo = 2 * np.random.rand(dim, dim).astype(np.float32) - 1
-qubo = (qubo + qubo.T) / 2
+# dim = 1050
+# window = 50
+# maxStep = 100000
+# qubo = 2 * np.random.rand(dim, dim).astype(np.float32) - 1
+# qubo = (qubo + qubo.T) / 2
+# qubo = qubo.flatten()
+# spin = 2 * np.random.rand(dim).astype(np.float32) - 1
+
+# test code
+dim = 5
+window = 50
+maxStep = 10000
+qubo = np.array([[-.1, .2, -.3, .4, -.5], [.2, .3, -.4, .5, .6], [-.3, -.4, -.5, -.6, -.7],
+                [.4, .5, -.6, .7, .8], [-.5, .6, -.7, .8, -.9]]).astype(np.float32)
 qubo = qubo.flatten()
-spin = np.ones(dim, dtype=np.int32)
-
+spin = 2 * np.random.rand(dim).astype(np.float32) - 1
+# test code
 
 spin = ctplib.as_ctypes(spin)
 qubo = ctplib.as_ctypes(qubo)
 
-simulatedQA = cdll.LoadLibrary("./lib/sqa.so")
+sbm = cdll.LoadLibrary("./lib/sbm.so")
 
-main = simulatedQA.simulatedQA
+main = sbm.iterate
 
-main.argtypes = [POINTER(c_int), POINTER(c_float), c_int, c_int, c_int]
-main.restype = c_float
-
+main.argtypes = [POINTER(c_float), POINTER(c_float), c_int, c_int, c_int]
+# main.restype = c_float
 
 start = time.time()
-energy = main(spin, qubo, dim, trotterNum, sweeps)
+# energy = main(spin, qubo, dim, window, maxStep)
+main(spin, qubo, dim, window, maxStep)
 end = time.time()
 
 spin = ctplib.as_array(spin)
+spin = np.expand_dims(np.sign(spin), axis=1)
+qubo = np.reshape(qubo, [dim, dim])
 
-print(f"%.9f" % energy)
+energy = -spin.T @ qubo @ spin
+
+print(energy)
 print(spin)
 print("spent time: ", end-start)
+
+# # test code
+# binary = np.expand_dims(binary, axis=1)
+# print( - binary.T @ qubo @ binary)
+# # test code
--- sbm.c
+++ sbm.c
@@ -4,24 +4,68 @@
 #include <math.h>
 
 #define TARGET_REPEAT 50
+#define TIME_STEP 0.01
+#define PRESSURE_SLOPE 0.01
+#define DETUNING_FREQUENCY 1
+#define HEAT_PARAMETER 0.06
 
-
-void update() {
-
+/**
+ * @brief
+ *
+ * @param arr
+ * @param size
+ * @return float
+ */
+float stddiv(float* arr, int size) {
+    float mean = 0;
+    for (int i = 0; i < size; i++) {
+        mean += arr[i];
+    }
+    mean /= size;
+    float sumDistanceSquare = 0;
+    for (int i = 0; i < size; i++) {
+        sumDistanceSquare += pow(arr[i] - mean, 2);
+    }
+    return sqrt(sumDistanceSquare / size);
 }
 
 /**
- * @brief 
- * 
- * @param spin 
- * @param dim 
+ * @brief
+ *
+ * @param spin
+ * @param momentum
+ * @param qubo
+ * @param dim
+ * @param step
+ * @param xi0
  */
-void confine(float* spin, int dim) {
+void update(float* spin, float* momentum, float* qubo, int dim, int step, float xi0) {
+    for (int i = 0; i < dim; i++) {
+        momentum[i] += TIME_STEP * (PRESSURE_SLOPE * TIME_STEP * step - DETUNING_FREQUENCY) * spin[i];
+        spin[i] += TIME_STEP * (PRESSURE_SLOPE * TIME_STEP * step + DETUNING_FREQUENCY - xi0 * qubo[i * dim + i]) * momentum[i];
+
+        float dot_product = 0;
+        for (int j = 0; j < dim; j++) {
+            dot_product += qubo[i * dim + j] * spin[j];
+        }
+        momentum[i] += TIME_STEP * (xi0 * dot_product + HEAT_PARAMETER * momentum[i]);
+    }
+}
+
+/**
+ * @brief
+ *
+ * @param spin
+ * @param dim
+ */
+void confine(float* spin, float* momentum, int dim) {
     for (int i = 0; i < dim; i++) {
         if (spin[i] < -1) {
             spin[i] = -1;
+            momentum[i] = 0;
         } else if (spin[i] > 1) {
             spin[i] = 1;
+            momentum[i] = 0;
         }
     }
 
@@ -44,38 +88,45 @@ int sameSpin(float* spin1, float* spin2, int dim) {
 }
 
 /**
- * @brief 
- * 
- * @param spin 
- * @param dim 
- * @param window 
- * @param maxStep 
+ * @brief
+ *
+ * @param spin
+ * @param dim
+ * @param window
+ * @param maxStep
  */
-void iterate(float* spin, int dim, int window, int maxStep) {
+extern void iterate(float* spin, float* qubo, int dim, int window, int maxStep) {
 
     if (window < 0) {
         printf("window cannot be negative.\n");
         exit(-1);
     }
 
-    switch (window) {
-    case 0:
-        for (int i = 0; i < maxStep; i++) {
-            update();
-            confine(spin, dim);
-        }
-        break;
-    default:
-        float** sample;
+    float* momentum;
+    momentum = (float*)malloc(dim * sizeof(float));
+
+    float xi0;
+    xi0 = (0.7 * DETUNING_FREQUENCY) / stddiv(qubo, dim * dim) * sqrt(dim);
+
+    float** sample;
+    if (window != 0) {
         sample = (float**)malloc((maxStep / window + 1) * sizeof(float*));
         for (int i = 0; i < maxStep / window + 1; i++) {
             sample[i] = (float*)malloc(dim * sizeof(float));
         }
+    }
 
-        int repeatNum = 0;
+    int repeatNum = 0;
+
+    if (window == 0) {
         for (int i = 0; i < maxStep; i++) {
-            update();
-            confine(spin, dim);
+            update(spin, momentum, qubo, dim, i, xi0);
+            confine(spin, momentum, dim);
+        }
+    } else {
+        for (int i = 0; i < maxStep; i++) {
+            update(spin, momentum, qubo, dim, i, xi0);
+            confine(spin, momentum, dim);
             if (i % window == 0) {
                 memcpy(sample[i / window], spin, dim * sizeof(float));
                 if (i != 0) {
@@ -87,11 +138,17 @@ void iterate(float* spin, int dim, int window, int maxStep) {
                 }
             }
         }
-
+    }
+    if (window != 0) {
         for (int i = 0; i < maxStep / window + 1; i++) {
             free(sample[i]);
         }
         free(sample);
-        break;
     }
+    free(momentum);
 }
+
+// int main(){
+
+//     iterate(float* spin, float* qubo, int dim, int window, int maxStep)
+// }
